33c2cb53cf43d1d38828ba02bfdfba1d88b130c2
kimkr <kimkr@kimkrui-MacBookPro.local>
2020-06-08 10:28:09 +0900

    wrong typing

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app = express();
 
-app.use(epxress.static('client/build'));
+app.use(express.static('client/build'));
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
