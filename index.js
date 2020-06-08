bb7b766b81157a3dd88361fbddd2170292c07ee7
kimkr <kimkr@kimkrui-MacBookPro.local>
2020-06-08 10:13:08 +0900

    init

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(epxress.static('client/build'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '/client', 'build', 'index.html'));
+});
+
+const PORT = process.env.PORT || 4000;
+
+const handleListening = () =>
+  console.log(`✅  Listening on: http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);
