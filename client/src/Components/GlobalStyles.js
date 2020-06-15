8b61d30b8cdbbe3fe15ea7833c75f9214af2217e
김경록 <kimkr@gimgyeonglog-ui-MacBookPro.local>
2020-06-15 17:14:11 +0900

    font5

--- client/src/Components/GlobalStyles.js
+++ client/src/Components/GlobalStyles.js
@@ -2,15 +2,15 @@ import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
 
 const globalStyles = createGlobalStyle`
-
     ${reset};
     a{
         text-decoration:none;
         color:inherit;
     }
     *{
+        font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
         box-sizing:border-box;
-        }
+    }
     body{
         font-size:10px;
     }
