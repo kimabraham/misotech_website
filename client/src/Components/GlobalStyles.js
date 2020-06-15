fe151de81161e88a75214cfb7ab2cfbc10edcbe5
김경록 <kimkr@gimgyeonglog-ui-MacBookPro.local>
2020-06-15 16:16:14 +0900

    font familly

--- client/src/Components/GlobalStyles.js
+++ client/src/Components/GlobalStyles.js
@@ -2,6 +2,7 @@ import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
 
 const globalStyles = createGlobalStyle`
+@font-face { font-family: 'NanumSquareRound'; src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_two@1.0/NanumSquareRound.woff') format('woff'); font-weight: normal; font-style: normal; }
     ${reset};
     a{
         text-decoration:none;
